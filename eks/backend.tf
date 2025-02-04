5cb8b8172e6322d1c62b04fbb84b6292d70d6b46
roammate-in <parag@roammate.in>
2025-02-04 21:17:13 +0530

    correcting bucketname

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "demo1212221" # Replace with your actual S3 bucket name
+    bucket = "jenkins-demo-backend-002" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
